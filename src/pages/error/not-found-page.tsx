5a84d7bb86b91026c37da81a257bf70a8897d19a
Miguel Fernandez <miguelferlez@gmail.com>
2025-05-21 18:02:58 +0200

    create not found page

---
+++ src/pages/error/not-found-page.tsx
@@ -0,0 +1,23 @@
+function NotFoundPage() {
+  return (
+    <section>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h2 className="mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
+            404
+          </h2>
+          <p className="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
+            Something's missing.
+          </p>
+          <p className="mb-4 text-lg font-light text-gray-500">
+            Sorry, we can't find that page. You'll find lots to explore on the
+            home page.
+          </p>
+          {/* TODO <Link to="/">Back to Homepage</Link> */}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default NotFoundPage;
