f23ff2e8f0af54a6ef8c5fc312e490eadf85d8eb
Miguel Fernandez <miguelferlez@gmail.com>
2025-05-22 19:00:07 +0200

    add routed app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
-import Header from "./components/layout/header";
+import AdvertsPage from "./pages/adverts/adverts-page";
+import NotFoundPage from "./pages/error/not-found-page";
+import Layout from "./components/layout/layout";
+import { Navigate, Route, Routes } from "react-router";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <Routes>
+      <Route path="/adverts" element={<Layout />}>
+        <Route index element={<AdvertsPage />} />
+      </Route>
+      <Route path="/" element={<Navigate to="/adverts" />} />
+      <Route path="/not-found" element={<NotFoundPage />} />
+      <Route path="*" element={<Navigate to="/not-found" />} />
+    </Routes>
   );
 }
 
