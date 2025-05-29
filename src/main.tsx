0827c377a15f801ea2ad689c891663c591a3ec0d
Miguel Fernandez <miguelferlez@gmail.com>
2025-05-29 14:43:08 +0200

    feat: add error boundary page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import ErrorBoundary from "./pages/error/error-boundary.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>,
 );
---
+++ src/pages/error/error-boundary.tsx
@@ -0,0 +1,65 @@
+import { Component, type ReactNode, type ErrorInfo } from "react";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  { error: Error | null; info: unknown }
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+
+    this.state = { error: null, info: null };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({ error, info: errorInfo });
+  }
+
+  render() {
+    const { error, info } = this.state;
+
+    return (
+      <>
+        {error ? (
+          <section>
+            <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+              <div className="mx-auto max-w-screen-sm text-center">
+                <h2 className="mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
+                  Oops!
+                </h2>
+                <p className="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
+                  Something went wrong.
+                </p>
+                <p className="mb-6 text-lg font-light text-gray-500">
+                  There was a problem processing the request, please try again
+                  later. If this error persists, don't hesitate to{" "}
+                  <a
+                    href="https://github.com/miguelferlez/keepcoding-practica-react/issues"
+                    target="blank"
+                    className="text-error hover:underline"
+                  >
+                    get in touch with us and open an issue for this project.
+                  </a>
+                </p>
+                <a href="/" className="btn btn-remove">
+                  Try again
+                </a>
+                <div className="mt-8 max-h-80 overflow-scroll rounded-xl bg-gray-100 p-4 text-left">
+                  <code>{error.message}</code>
+                  <code>{JSON.stringify(info)}</code>
+                </div>
+              </div>
+            </div>
+          </section>
+        ) : (
+          this.props.children
+        )}
+      </>
+    );
+  }
+}
+
+export default ErrorBoundary;
