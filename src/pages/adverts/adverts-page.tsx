21c3d69b0e7a8ce81453afb191fcbcb909a9f45f
Miguel Fernandez <miguelferlez@gmail.com>
2025-06-01 19:00:49 +0200

    feat: add name and tag filters in adverts page

--- src/pages/adverts/adverts-page.tsx
+++ src/pages/adverts/adverts-page.tsx
@@ -3,37 +3,108 @@ import Page from "../../components/layout/page";
 import { EmptyAdvertsWarning } from "./empty-adverts-warning";
 import { NotFoundAdvertsWarning } from "./not-found-adverts-warning";
 import { Link } from "react-router";
-import { getAdverts } from "../../services/adverts";
+import { getAdverts, getAdvertTags } from "../../services/adverts";
 import type { Advert } from "./types";
 import AdvertCard from "./advert-card";
+import FormField from "../../components/ui/form-field";
+import MagnifyingGlassIcon from "../../components/icons/magnifying-glass-icon";
+import clsx from "clsx";
+import ChoiceField from "../../components/ui/choice-field";
 
 function AdvertsPage() {
   const [adverts, setAdverts] = useState<Advert[]>([]);
+  const [advertTags, setAdvertTags] = useState<String[]>([]);
+  const [search, setSearch] = useState("");
+  const [selectedTags, setSelectedTags] = useState<String[]>([]);
+
+  const filterAdverts = adverts.filter((advert) => {
+    const advertName = advert.name.toLowerCase();
+    const advertTags = advert.tags;
+    return (
+      advertName.includes(search.toLowerCase()) &&
+      selectedTags.every((tag) => advertTags.includes(`${tag}`))
+    );
+  });
+
+  const toggleTag = (tagValue: string) => {
+    setSelectedTags((prevTags) =>
+      prevTags.includes(tagValue)
+        ? prevTags.filter((tag) => tag !== tagValue)
+        : [...prevTags, tagValue],
+    );
+  };
 
   useEffect(() => {
     async function fetchAdverts() {
       const adverts = await getAdverts();
+      const advertTags = await getAdvertTags();
       setAdverts(adverts);
+      setAdvertTags(advertTags);
     }
     fetchAdverts();
   }, []);
 
   return (
-    <Page title="Newsfeed">
-      {adverts.length ? (
-        <ul className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {adverts.map((advert) => (
-            <li key={advert.id}>
-              <Link to={`/adverts/${advert.id}`}>
-                <AdvertCard advert={advert} />
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <EmptyAdvertsWarning />
-      )}
-    </Page>
+    <>
+      <div className="mb-4 border-b-1 border-gray-200">
+        <form className="container mx-auto items-center gap-4 p-6 sm:flex sm:py-3">
+          <div className="grow-4">
+            <FormField
+              label=""
+              type="text"
+              name="search"
+              value={search}
+              icon={<MagnifyingGlassIcon className="size-6 text-gray-400" />}
+              onChange={(event) => {
+                setSearch(event.target.value);
+              }}
+              placeholder="Search products"
+            />
+          </div>
+          <div className="grow">
+            <ul className="mb-2 w-full items-center rounded-[9px] border border-gray-200 text-sm sm:flex">
+              {advertTags.map((tag) => (
+                <ChoiceField
+                  label={`${tag}`}
+                  type="checkbox"
+                  name="tag"
+                  value={`${tag}`}
+                  forId={`${tag}`}
+                  key={`advert-tag-${tag}`}
+                  onChange={() => {
+                    toggleTag(`${tag}`);
+                  }}
+                />
+              ))}
+            </ul>
+          </div>
+        </form>
+      </div>
+      <Page title="Newsfeed">
+        {adverts.length ? (
+          <ul
+            className={clsx({
+              "grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8":
+                filterAdverts.length !== 0,
+            })}
+          >
+            {filterAdverts.length ? (
+              filterAdverts.map((advert) => (
+                <li key={advert.id}>
+                  <Link to={`/adverts/${advert.id}`}>
+                    <AdvertCard advert={advert} />
+                  </Link>
+                </li>
+              ))
+            ) : (
+              <NotFoundAdvertsWarning />
+            )}
+          </ul>
+        ) : (
+          <EmptyAdvertsWarning />
+        )}
+      </Page>
+    </>
   );
 }
 
--- src/pages/adverts/not-found-adverts-warning.tsx
+++ src/pages/adverts/not-found-adverts-warning.tsx
@@ -1,13 +1,13 @@
 import MagnifyingGlassIcon from "../../components/icons/magnifying-glass-icon";
 
 export const NotFoundAdvertsWarning = () => (
-  <section>
-    <div className="mx-auto flex h-[calc(100vh-300px)] w-full max-w-sm flex-col px-4 text-center">
+  <section className="">
+    <div className="mx-auto flex h-[calc(100vh-300px)] max-w-sm flex-col items-center px-4">
       <div className="mx-auto mb-3 rounded-full bg-blue-100 p-3">
         <MagnifyingGlassIcon className="size-6 text-blue-500" />
       </div>
       <h1 className="mb-2 text-lg text-gray-800">There's no advert here</h1>
-      <p className="mb-4 text-gray-500">
+      <p className="mb-4 text-center text-gray-500">
         Try changing your filters to see appointments
       </p>
     </div>
